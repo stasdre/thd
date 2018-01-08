00a168e11b3e4240f4a16610bc78e73ddf663842
Stan <stasdre@gmail.com>
2018-01-08 21:28:29 +0200

    clean admin template and new organizations views

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,11 @@
 @extends('admin.layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="box box-default">
+        <div class="box-body">
+            Content dashboard
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,13 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MAIN NAVIGATION</li>
+            <li>
+                <a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
