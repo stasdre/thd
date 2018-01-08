c33a8540547abd695f06b1d4d455aeca4d7496bf
Stan <stasdre@gmail.com>
2018-01-08 13:41:59 +0200

    install zizaco/entrust package for users permissions

---
+++ app/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Thd;
+
+use Zizaco\Entrust\EntrustPermission;
+
+class Permission extends EntrustPermission
+{
+    //
+}
--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // Thd\Providers\BroadcastServiceProvider::class,
         Thd\Providers\EventServiceProvider::class,
         Thd\Providers\RouteServiceProvider::class,
-
+        Zizaco\Entrust\EntrustServiceProvider::class,
     ],
 
     /*
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
     ],
 
 ];
