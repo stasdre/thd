0038ea4766cbcf1e2737178d5ebcaa942c44d55a
Stan <stasdre@gmail.com>
2018-01-09 19:19:45 +0200

    add relationships one-to-many for Plans and User

--- app/Plan.php
+++ app/Plan.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    /**
+     * Get the user that owns the house plan.
+     */
+    public function user()
+    {
+        return $this->belongsTo('Thd\User');
+    }
 }
