d932b77404806456826f7ca92afc2129fd6b187b
Stan <stasdre@gmail.com>
2018-01-24 18:17:01 +0200

    users and roles seeders

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id'=>1,
+                'name'=>'Admin',
+                'email'=>'admin@admin.com',
+                'password'=>bcrypt('123456'),
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ]);
+    }
+}
