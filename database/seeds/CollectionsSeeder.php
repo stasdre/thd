d7ef46d1c2798f0b208a5133a92ed1dbf6e53cff
Stan <stasdre@gmail.com>
2018-01-24 18:25:01 +0200

    seeds for styles and collections

---
+++ database/seeds/CollectionsSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+class CollectionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('collections')->insert([
+            [
+                'name'=>'1 1/2 Story Home Plans',
+                'short_name'=>'1 1/2',
+                'description'=>'description',
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name'=>'3D Plans',
+                'short_name'=>'3D',
+                'description'=>'description',
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name'=>'Accessible Floor Plans',
+                'short_name'=>'Accessible',
+                'description'=>'description',
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name'=>'Additions',
+                'short_name'=>'Additions',
+                'description'=>'description',
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name'=>'Affordable Plans',
+                'short_name'=>'Affordable',
+                'description'=>'description',
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name'=>'Bi-Level Home Plans',
+                'short_name'=>'Bi-Level',
+                'description'=>'description',
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name'=>'Builder Preferred Plans',
+                'short_name'=>'Builder Preferred',
+                'description'=>'description',
+                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at'=>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+        ]);
+    }
+}
