58824439eea3ef56265fdb6145fd5fa5827cc434
Stan <stasdre@gmail.com>
2018-03-07 10:30:53 +0200

    pricing options

--- resources/views/admin/_plans_menu.blade.php
+++ resources/views/admin/_plans_menu.blade.php
@@ -4,7 +4,7 @@
         <li role="presentation" class="{{ $active == 'graphics' ? 'active' : '' }}"><a href="{{ route('plan-images.create', [$plan]) }}">Graphics</a></li>
         <li role="presentation" class="{{ $active == 'features' ? 'active' : '' }}"><a href="{{ route('plan-features.create', [$plan]) }}">Key Features</a></li>
         <li role="presentation" class="{{ $active == 'desc' ? 'active' : '' }}"><a href="{{ route('plan-desc.create', [$plan]) }}">Plan Description</a></li>
-        <li role="presentation" class=""><a href="#">Pricing Options</a></li>
+        <li role="presentation" class="{{ $active == 'pricing' ? 'active' : '' }}"><a href="{{ route('plan-packages.create', [$plan]) }}">Pricing Options</a></li>
         <li role="presentation" class=""><a href="#">Preview Plan Page</a></li>
     @else
         <li role="presentation" class="active"><a href="{{ route('house-plan.create') }}">Plan Detail</a></li>
--- resources/views/admin/plan-package/create.blade.php
+++ resources/views/admin/plan-package/create.blade.php
@@ -6,24 +6,22 @@
 
 @section('content')
     <div class="box box-default">
-        {!! Form::open(['route' => ['plan-packages.store', $plan->id], 'class' => 'form-horizontal', 'method' => 'post']) !!}
         <div class="box-header with-border">
-            <div class="row">
-                <div class="col-sm-6">
-                    <h3 style="margin-top: 0px;">Plan Packages</h3>
-                </div>
-                <div class="col-sm-6">
-                    <button type="submit" class="btn btn-success pull-right">Save</button>
-                </div>
+            <div class="col-sm-8">
+                @include('admin._plans_menu', ['active'=>'pricing', 'plan'=>$plan->id])
+            </div>
+            <div class="col-sm-4" style="padding-top: 5px;">
+                <a class="btn btn-default" href="{{ route('house-plan.index') }}" role="button">Cancel</a>
+                <a role="button" id="plans-submit-close" href="#" class="btn btn-warning">Save & Close</a>
+                <a role="button" id="plans-submit-next" href="#" class="btn btn-success">Save & Next step</a>
             </div>
         </div>
         <div class="box-body">
-            @include('admin.plan-package._form')
+            {!! Form::open(['route' => ['plan-packages.store', $plan->id], 'class' => 'form-horizontal', 'method' => 'post']) !!}
+                @include('admin.plan-package._form')
+            {!! Form::close() !!}
         </div>
         <div class="box-footer">
-            <a class="btn btn-default" href="{{ route('house-plan.index') }}" role="button">Cancel</a>
-            <button type="submit" class="btn btn-success pull-right">Save</button>
         </div>
-        {!! Form::close() !!}
     </div>
 @endsection
