cdec09bba1736a7c0566287f01375fa0de7d6e28
Stan <stasdre@gmail.com>
2019-02-12 11:04:55 +0200

    fix typo

--- resources/views/layouts/best-seller-sidebar.blade.php
+++ resources/views/layouts/best-seller-sidebar.blade.php
@@ -47,7 +47,7 @@
             <div class="text-center blackBanner">
                 <h3 class="font-futura">SIGN UP + SAVE!</h3>
                 <span class="font-futura bigFont">10%</span> &nbsp;&nbsp;<span style="font-size: 31px;">off</span>
-                <p>Plus, get exclusice access to new <br> house plans and great product ideas!</p>
+                <p>Plus, get exclusive access to new <br> house plans and great product ideas!</p>
                 <input name="email" class="email_class" type="text"  placeholder="Enter your email address"><br>
                 <button class="coupon_code" >GET MY COUPON</button>
             </div>
