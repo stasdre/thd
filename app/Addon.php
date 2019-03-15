5ce47b05c5008ba01227233c48a2eec190e1c4a5
Stan <stasdre@gmail.com>
2019-03-15 09:50:46 +0200

    make fillable short_desc

--- app/Addon.php
+++ app/Addon.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Addon extends Model
 {
-    protected $fillable = ['name', 'description'];
+    protected $fillable = ['name', 'description', 'short_desc'];
 }
