9f2f8fc5952174c7b8ea360a2f7e433b7ae87337
Stan <stasdre@gmail.com>
2019-11-12 21:09:41 +0200

    search result slider ordering

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -14,155 +14,160 @@ use Validator;
 class PlanController extends Controller
 {
 
-    public function view(Request $request, $plan_number)
-    {
-        $plan = Plan::where('plan_number', $plan_number)
-            ->with('packages')
-            ->with('foundationOptions')
-            ->with('addons')
-            ->with(['images' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with('roomsInterior')
-            ->with('outdoors')
-            ->with('beds')
-            ->with('kitchens')
-            ->with('styles')
-            ->with(['images_first' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_second' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_basement' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_bonus' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_cars' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }]);
+  public function view(Request $request, $plan_number)
+  {
+    $plan = Plan::where('plan_number', $plan_number)
+      ->with('packages')
+      ->with('foundationOptions')
+      ->with('addons')
+      ->with(['images' => function ($query) {
+        $query->orderBy('for_search', 'desc');
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with('roomsInterior')
+      ->with('outdoors')
+      ->with('beds')
+      ->with('kitchens')
+      ->with('styles')
+      ->with(['images_first' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_second' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_basement' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_bonus' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_cars' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }]);
 
-        if(!Auth::user() || Auth::user()->hasRole('customer'))
-            $plan->where('is_active', 1);
+    if (!Auth::user() || Auth::user()->hasRole('customer'))
+      $plan->where('is_active', 1);
 
-        $dataPlan = $plan->firstOrFail();
+    $dataPlan = $plan->firstOrFail();
 
-        $dataPlan->views += 1;
-        $dataPlan->update();
+    $dataPlan->views += 1;
+    $dataPlan->update();
 
-        /*$packages = Package::whereHas('plans', function($query) use ($plan){
+    /*$packages = Package::whereHas('plans', function($query) use ($plan){
             $query->where('plan_id', '=', $plan->id);
         })->get();*/
-        $packages = [];
-        if($dataPlan->packages){
-            foreach($dataPlan->packages as $package){
-                $packages[] = [
-                    'id'=>$package->id,
-                    'name'=>$package->name,
-                    'price'=>$package->pivot->price,
-                    'desc'=>$package->short_desc,
-                ];
-            }
-        }
-
-        $foundations = [];
-        if($dataPlan->foundationOptions){
-            foreach($dataPlan->foundationOptions as $foundation){
-                $foundations[] = [
-                    'id'=>$foundation->id,
-                    'name'=>$foundation->name,
-                    'price'=>$foundation->pivot->price,
-                    'desc'=>$foundation->short_desc,
-                ];
-            }
-        }
-
-        $addons = [];
-        if($dataPlan->addons){
-            foreach($dataPlan->addons as $addon){
-                $addons[] = [
-                    'id'=>$addon->id,
-                    'name'=>$addon->name,
-                    'price'=>$addon->pivot->price,
-                    'desc'=>$addon->short_desc,
-                ];
-            }
-        }
-
-        return view('plan.plan', [
-            'plan'=>$dataPlan,
-            'packages'=>json_encode($packages),
-            'foundations'=>json_encode($foundations),
-            'addons'=>json_encode($addons)
-        ]);
-    }
-    
-    public function modifyplan(Request $request, $plan_number){
-
-        $plan = Plan::where('plan_number', $plan_number)
-            ->with(['images' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_first' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_second' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_basement' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_bonus' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }])
-            ->with(['images_cars' => function($query){
-                $query->orderBy('sort_number', 'ASC');
-            }]);
-
-        if(!Auth::user() || Auth::user()->hasRole('customer'))
-            $plan->where('is_active', 1);
-
-        $dataPlan = $plan->firstOrFail();
-
-		return view('plan.modify-plan', [
-            'plan'=>$dataPlan,
-            'states'=>State::orderBy('name')->pluck('name', 'abbr')->toArray()
-        ]);
+    $packages = [];
+    if ($dataPlan->packages) {
+      foreach ($dataPlan->packages as $package) {
+        $packages[] = [
+          'id' => $package->id,
+          'name' => $package->name,
+          'price' => $package->pivot->price,
+          'desc' => $package->short_desc,
+        ];
+      }
     }
 
-    public function modifyplanpost(Request $request, $plan_number){
-        $validator = Validator::make($request->all(), [
-            'first_name' => 'required',
-            'last_name' => 'required',
-            'email' => 'required|email',
-            'phone'=>'required',
-            'state'=>'required',
-            'foundation'=>'required',
-            'builder'=>'required',
-            'framing'=>'required',
-            'message'=>'required',
-            'files'=>'required|file',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect(route('modify-plan', $plan_number))
-                ->withErrors($validator)
-                ->withInput();
-        }else{
-            //-----send email-----//
-            return redirect()->route('modify-plan', $plan_number)->with('message', [
-                'type'=>'success',
-                'title'=>'Success!',
-                'message'=>'Request was send']);
-        }
+    $foundations = [];
+    if ($dataPlan->foundationOptions) {
+      foreach ($dataPlan->foundationOptions as $foundation) {
+        $foundations[] = [
+          'id' => $foundation->id,
+          'name' => $foundation->name,
+          'price' => $foundation->pivot->price,
+          'desc' => $foundation->short_desc,
+        ];
+      }
     }
 
-    public function all(){
-        $allCollections = Collection::orderBy('name', 'asc')->where('is_active', 1)->get();
-        $allStyles = Style::orderBy('name', 'asc')->where('is_active', 1)->get();
-
-        return view('plan.all', ['allCollections'=>$allCollections, 'allStyles'=>$allStyles]);
+    $addons = [];
+    if ($dataPlan->addons) {
+      foreach ($dataPlan->addons as $addon) {
+        $addons[] = [
+          'id' => $addon->id,
+          'name' => $addon->name,
+          'price' => $addon->pivot->price,
+          'desc' => $addon->short_desc,
+        ];
+      }
     }
+
+    return view('plan.plan', [
+      'plan' => $dataPlan,
+      'packages' => json_encode($packages),
+      'foundations' => json_encode($foundations),
+      'addons' => json_encode($addons)
+    ]);
+  }
+
+  public function modifyplan(Request $request, $plan_number)
+  {
+
+    $plan = Plan::where('plan_number', $plan_number)
+      ->with(['images' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_first' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_second' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_basement' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_bonus' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }])
+      ->with(['images_cars' => function ($query) {
+        $query->orderBy('sort_number', 'ASC');
+      }]);
+
+    if (!Auth::user() || Auth::user()->hasRole('customer'))
+      $plan->where('is_active', 1);
+
+    $dataPlan = $plan->firstOrFail();
+
+    return view('plan.modify-plan', [
+      'plan' => $dataPlan,
+      'states' => State::orderBy('name')->pluck('name', 'abbr')->toArray()
+    ]);
+  }
+
+  public function modifyplanpost(Request $request, $plan_number)
+  {
+    $validator = Validator::make($request->all(), [
+      'first_name' => 'required',
+      'last_name' => 'required',
+      'email' => 'required|email',
+      'phone' => 'required',
+      'state' => 'required',
+      'foundation' => 'required',
+      'builder' => 'required',
+      'framing' => 'required',
+      'message' => 'required',
+      'files' => 'required|file',
+    ]);
+
+    if ($validator->fails()) {
+      return redirect(route('modify-plan', $plan_number))
+        ->withErrors($validator)
+        ->withInput();
+    } else {
+      //-----send email-----//
+      return redirect()->route('modify-plan', $plan_number)->with('message', [
+        'type' => 'success',
+        'title' => 'Success!',
+        'message' => 'Request was send'
+      ]);
+    }
+  }
+
+  public function all()
+  {
+    $allCollections = Collection::orderBy('name', 'asc')->where('is_active', 1)->get();
+    $allStyles = Style::orderBy('name', 'asc')->where('is_active', 1)->get();
+
+    return view('plan.all', ['allCollections' => $allCollections, 'allStyles' => $allStyles]);
+  }
 }
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,208 +16,210 @@ use Thd\Style;
 
 class SearchController extends Controller
 {
-    public function index(Request $request)
-    {
-        return view('search.index');
+  public function index(Request $request)
+  {
+    return view('search.index');
+  }
+
+  public function advanced(Request $request)
+  {
+    $beds = Bed::orderBy('name')->get();
+    $kitchens = Kitchen::orderBy('name')->get();
+    $roomsInteriors = RoomInterior::orderBy('name')->get();
+    $garages = Garage::orderBy('name')->get();
+    $outdoors = Outdoor::orderBy('name')->get();
+
+    $styles = Style::orderBy('name')->get();
+    $collections = Collection::orderBy('name')->get();
+
+    $mostPlans = Plan::orderBy('views', 'desc')->limit(10)->with(['images' => function ($query) {
+      $query->orderBy('for_search', 'desc');
+      $query->orderBy('sort_number', 'asc');
+    }])->get();
+
+
+    return view('search.advanced', [
+      'beds' => $beds,
+      'kitchens' => $kitchens,
+      'roomsInteriors' => $roomsInteriors,
+      'garages' => $garages,
+      'outdoors' => $outdoors,
+      'styles' => $styles,
+      'collections' => $collections,
+      'mostPlans' => $mostPlans
+    ]);
+  }
+
+  public function result(Request $request)
+  {
+    $input = $request->all();
+
+    $views = $request->get('views');
+    $collection = $request->get('collection');
+    $style = $request->get('style');
+
+    $order = $request->get('order') ? $request->get('order') : 'popular';
+
+    $sq_min = $request->get('sq_min');
+    $sq_max = $request->get('sq_max');
+    $beds = $request->get('beds');
+    $baths = $request->get('baths');
+    $garages = $request->get('garages');
+    $stories = $request->get('stories');
+    $txt = $request->get('txt');
+    $width_min = $request->get('width_min');
+    $width_max = $request->get('width_max');
+    $depth_min = $request->get('depth_min');
+    $depth_max = $request->get('depth_max');
+
+    $bf = $request->get('bf');
+    $kf = $request->get('kf');
+    $rf = $request->get('rf');
+    $gf = $request->get('gf');
+    $ef = $request->get('ef');
+    $styles = $request->get('styles');
+    $collections = $request->get('collections');
+
+    $rules = [
+      'views' => 'required|in:24,50',
+      'order' => 'nullable|in:popular,recent,s_l,l_s',
+      'collection' => 'nullable|integer',
+      'style' => 'nullable|integer',
+      'sq_min' => 'nullable|numeric',
+      'sq_max' => 'nullable|numeric',
+      'beds' => 'nullable|numeric',
+      'baths' => 'nullable|numeric',
+      'garages' => 'nullable|numeric',
+      'stories' => 'nullable|numeric',
+      'txt' => 'nullable|string',
+      'width_min' => 'nullable|numeric',
+      'width_max' => 'nullable|numeric',
+      'depth_min' => 'nullable|numeric',
+      'depth_max' => 'nullable|numeric',
+    ];
+
+
+    $validation = Validator::make($input, $rules);
+
+    if ($validation->fails()) {
+      return abort(404);
+    }
+    $plans = Plan::where('is_active', '=', 1);
+
+    if (!empty($collection)) {
+      $plans->whereHas('collections', function ($query) use ($collection) {
+        $query->where('collection_id', '=', $collection);
+      });
     }
 
-    public function advanced(Request $request)
-    {
-        $beds = Bed::orderBy('name')->get();
-        $kitchens = Kitchen::orderBy('name')->get();
-        $roomsInteriors = RoomInterior::orderBy('name')->get();
-        $garages = Garage::orderBy('name')->get();
-        $outdoors = Outdoor::orderBy('name')->get();
-
-        $styles = Style::orderBy('name')->get();
-        $collections = Collection::orderBy('name')->get();
-
-        $mostPlans = Plan::orderBy('views', 'desc')->limit(10)->with(['images' => function ($query) {
-            $query->where('for_search', 1);
-        }])->get();
-
-
-        return view('search.advanced', [
-            'beds' => $beds,
-            'kitchens' => $kitchens,
-            'roomsInteriors' => $roomsInteriors,
-            'garages' => $garages,
-            'outdoors' => $outdoors,
-            'styles' => $styles,
-            'collections' => $collections,
-            'mostPlans' => $mostPlans
-        ]);
+    if (!empty($style)) {
+      $plans->whereHas('styles', function ($query) use ($style) {
+        $query->where('style_id', '=', $style);
+      });
     }
 
-    public function result(Request $request)
-    {
-        $input = $request->all();
+    if ($sq_min)
+      $plans->where('square_ft->str_total', '>=', $sq_min);
 
-        $views = $request->get('views');
-        $collection = $request->get('collection');
-        $style = $request->get('style');
+    if ($sq_max)
+      $plans->where('square_ft->str_total', '<=', $sq_max);
 
-        $order = $request->get('order') ? $request->get('order') : 'popular';
+    if ($beds)
+      $plans->where('rooms->r_bedrooms', '>=', $beds);
 
-        $sq_min = $request->get('sq_min');
-        $sq_max = $request->get('sq_max');
-        $beds = $request->get('beds');
-        $baths = $request->get('baths');
-        $garages = $request->get('garages');
-        $stories = $request->get('stories');
-        $txt = $request->get('txt');
-        $width_min = $request->get('width_min');
-        $width_max = $request->get('width_max');
-        $depth_min = $request->get('depth_min');
-        $depth_max = $request->get('depth_max');
+    if ($baths)
+      $plans->where('rooms->r_full_baths', '>=', $baths);
 
-        $bf = $request->get('bf');
-        $kf = $request->get('kf');
-        $rf = $request->get('rf');
-        $gf = $request->get('gf');
-        $ef = $request->get('ef');
-        $styles = $request->get('styles');
-        $collections = $request->get('collections');
+    if ($garages)
+      $plans->where('garage->car', '>=', $garages);
 
-        $rules = [
-            'views' => 'required|in:24,50',
-            'order' => 'nullable|in:popular,recent,s_l,l_s',
-            'collection' => 'nullable|integer',
-            'style' => 'nullable|integer',
-            'sq_min' => 'nullable|numeric',
-            'sq_max' => 'nullable|numeric',
-            'beds' => 'nullable|numeric',
-            'baths' => 'nullable|numeric',
-            'garages' => 'nullable|numeric',
-            'stories' => 'nullable|numeric',
-            'txt' => 'nullable|string',
-            'width_min' => 'nullable|numeric',
-            'width_max' => 'nullable|numeric',
-            'depth_min' => 'nullable|numeric',
-            'depth_max' => 'nullable|numeric',
-        ];
+    if ($stories)
+      $plans->where('dimensions->stories', '>=', $stories);
 
-
-        $validation = Validator::make($input, $rules);
-
-        if ($validation->fails()) {
-            return abort(404);
-        }
-        $plans = Plan::where('is_active', '=', 1);
-
-        if (!empty($collection)) {
-            $plans->whereHas('collections', function ($query) use ($collection) {
-                $query->where('collection_id', '=', $collection);
-            });
-        }
-
-        if (!empty($style)) {
-            $plans->whereHas('styles', function ($query) use ($style) {
-                $query->where('style_id', '=', $style);
-            });
-        }
-
-        if ($sq_min)
-            $plans->where('square_ft->str_total', '>=', $sq_min);
-
-        if ($sq_max)
-            $plans->where('square_ft->str_total', '<=', $sq_max);
-
-        if ($beds)
-            $plans->where('rooms->r_bedrooms', '>=', $beds);
-
-        if ($baths)
-            $plans->where('rooms->r_full_baths', '>=', $baths);
-
-        if ($garages)
-            $plans->where('garage->car', '>=', $garages);
-
-        if ($stories)
-            $plans->where('dimensions->stories', '>=', $stories);
-
-        if ($txt) {
-            $plans->where(function ($query) use ($txt) {
-                $query->where('name', 'like', '%' . $txt . '%')
-                    ->orWhere('plan_number', 'like', '%' . $txt . '%');
-            });
-        }
-
-        if ($width_min)
-            $plans->where('dimensions->width_ft', '>=', $width_min);
-
-        if ($width_max)
-            $plans->where('dimensions->width_ft', '<=', $width_max);
-
-
-        if ($depth_min)
-            $plans->where('dimensions->depth_ft', '>=', $depth_min);
-
-        if ($depth_max)
-            $plans->where('dimensions->depth_ft', '<=', $depth_max);
-
-
-        if (!empty($bf)) {
-            $plans->whereHas('beds', function ($query) use ($bf) {
-                $query->whereIn('bed_id', explode(",", $bf));
-            });
-        }
-
-        if (!empty($kf)) {
-            $plans->whereHas('kitchens', function ($query) use ($kf) {
-                $query->whereIn('kitchen_id', explode(",", $kf));
-            });
-        }
-
-
-        if (!empty($rf)) {
-            $plans->whereHas('roomsInterior', function ($query) use ($rf) {
-                $query->whereIn('room_interior_id', explode(",", $rf));
-            });
-        }
-
-        if (!empty($gf)) {
-            $plans->whereHas('garages', function ($query) use ($gf) {
-                $query->whereIn('garage_id', explode(",", $gf));
-            });
-        }
-
-        if (!empty($ef)) {
-            $plans->whereHas('outdoors', function ($query) use ($ef) {
-                $query->whereIn('outdoor_id', explode(",", $ef));
-            });
-        }
-
-        if (!empty($styles)) {
-            $plans->whereHas('styles', function ($query) use ($styles) {
-                $query->whereIn('style_id', explode(",", $styles));
-            });
-        }
-
-        if (!empty($collections)) {
-            $plans->whereHas('collections', function ($query) use ($collections) {
-                $query->whereIn('collection_id', explode(",", $collections));
-            });
-        }
-
-        switch ($order) {
-            case 'popular':
-                $plans->orderBy('views', 'desc');
-                break;
-            case 'recent':
-                $plans->orderBy('created_at', 'desc');
-                break;
-            case 's_l':
-                $plans->orderBy('square_ft->str_total', 'asc');
-                break;
-            case 'l_s':
-                $plans->orderBy('square_ft->str_total', 'desc');
-                break;
-        }
-
-        $dataPlans = $plans->with(['images' => function ($query) {
-            $query->where('for_search', 1);
-        }])->paginate($views);
-        //return view('search.index', ['plans'=>$dataPlans]);
-        return response()->json($dataPlans);
+    if ($txt) {
+      $plans->where(function ($query) use ($txt) {
+        $query->where('name', 'like', '%' . $txt . '%')
+          ->orWhere('plan_number', 'like', '%' . $txt . '%');
+      });
     }
+
+    if ($width_min)
+      $plans->where('dimensions->width_ft', '>=', $width_min);
+
+    if ($width_max)
+      $plans->where('dimensions->width_ft', '<=', $width_max);
+
+
+    if ($depth_min)
+      $plans->where('dimensions->depth_ft', '>=', $depth_min);
+
+    if ($depth_max)
+      $plans->where('dimensions->depth_ft', '<=', $depth_max);
+
+
+    if (!empty($bf)) {
+      $plans->whereHas('beds', function ($query) use ($bf) {
+        $query->whereIn('bed_id', explode(",", $bf));
+      });
+    }
+
+    if (!empty($kf)) {
+      $plans->whereHas('kitchens', function ($query) use ($kf) {
+        $query->whereIn('kitchen_id', explode(",", $kf));
+      });
+    }
+
+
+    if (!empty($rf)) {
+      $plans->whereHas('roomsInterior', function ($query) use ($rf) {
+        $query->whereIn('room_interior_id', explode(",", $rf));
+      });
+    }
+
+    if (!empty($gf)) {
+      $plans->whereHas('garages', function ($query) use ($gf) {
+        $query->whereIn('garage_id', explode(",", $gf));
+      });
+    }
+
+    if (!empty($ef)) {
+      $plans->whereHas('outdoors', function ($query) use ($ef) {
+        $query->whereIn('outdoor_id', explode(",", $ef));
+      });
+    }
+
+    if (!empty($styles)) {
+      $plans->whereHas('styles', function ($query) use ($styles) {
+        $query->whereIn('style_id', explode(",", $styles));
+      });
+    }
+
+    if (!empty($collections)) {
+      $plans->whereHas('collections', function ($query) use ($collections) {
+        $query->whereIn('collection_id', explode(",", $collections));
+      });
+    }
+
+    switch ($order) {
+      case 'popular':
+        $plans->orderBy('views', 'desc');
+        break;
+      case 'recent':
+        $plans->orderBy('created_at', 'desc');
+        break;
+      case 's_l':
+        $plans->orderBy('square_ft->str_total', 'asc');
+        break;
+      case 'l_s':
+        $plans->orderBy('square_ft->str_total', 'desc');
+        break;
+    }
+
+    $dataPlans = $plans->with(['images' => function ($query) {
+      $query->orderBy('for_search', 'desc');
+      $query->orderBy('sort_number', 'asc');
+    }])->paginate($views);
+    //return view('search.index', ['plans'=>$dataPlans]);
+    return response()->json($dataPlans);
+  }
 }
