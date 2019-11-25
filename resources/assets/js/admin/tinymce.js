41c5a7e7037ac9d1e897beba1e3ce4be703f787b
Stan <stasdre@gmail.com>
2019-11-25 11:21:40 +0200

    add filemanager

--- resources/assets/js/admin/tinymce.js
+++ resources/assets/js/admin/tinymce.js
@@ -33,7 +33,8 @@ import "tinymce/plugins/paste/plugin";
 import "tinymce/plugins/textcolor/plugin";
 
 // Initialize
-tinymce.init({
+var editor_config = {
+  path_absolute: "/",
   selector: ".tinymce-editor",
   menubar: false,
   plugins: [
@@ -42,5 +43,37 @@ tinymce.init({
     "save table contextmenu directionality template paste textcolor"
   ],
   toolbar:
-    "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor | fontsizeselect | fontselect"
-});
+    "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor | fontsizeselect | fontselect",
+  relative_urls: false,
+  file_browser_callback: function(field_name, url, type, win) {
+    var x =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.getElementsByTagName("body")[0].clientWidth;
+    var y =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.getElementsByTagName("body")[0].clientHeight;
+
+    var cmsURL =
+      editor_config.path_absolute +
+      "laravel-filemanager?field_name=" +
+      field_name;
+    if (type == "image") {
+      cmsURL = cmsURL + "&type=Images";
+    } else {
+      cmsURL = cmsURL + "&type=Files";
+    }
+
+    tinyMCE.activeEditor.windowManager.open({
+      file: cmsURL,
+      title: "Filemanager",
+      width: x * 0.8,
+      height: y * 0.8,
+      resizable: "yes",
+      close_previous: "no"
+    });
+  }
+};
+
+tinymce.init(editor_config);
