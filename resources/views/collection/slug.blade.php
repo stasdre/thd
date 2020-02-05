ad19cc79d1151b434fa990c703348bd945391f77
Stan <stasdre@gmail.com>
2020-02-05 16:55:49 +0200

    collection page meta tags

--- resources/views/collection/slug.blade.php
+++ resources/views/collection/slug.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.index')
 
+@section('title', $collection->meta_title)
+@section('description', $collection->meta_description)
+@section('keywords', $collection->meta_keywords)
+
 @section('content')
 <div class="row align-items-center style-desc-container">
   <div class="col-sm-7">
