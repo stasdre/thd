61face1da9059e0b8f1fd7187e2eb7184b1fff00
Stan <stasdre@gmail.com>
2020-03-02 15:46:32 +0200

    fix vue default export

--- resources/assets/js/plans-search.js
+++ resources/assets/js/plans-search.js
@@ -1,8 +1,8 @@
 window.Vue = require("vue");
 
-Vue.component("plans-list", require("./components/PlansList.vue"));
-Vue.component("promo", require("./components/Promo.vue"));
+Vue.component("plans-list", require("./components/PlansList.vue").default);
+Vue.component("promo", require("./components/Promo.vue").default);
 
 const plans = new Vue({
-  el: "#plans-search"
+    el: "#plans-search"
 });
